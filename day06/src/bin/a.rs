6ab15d562d4867d053b7a12b21e5800102b9fb29
Richard Körner <grasbock@gmail.com>
2022-12-18 12:48:52 +0100

    refactor

--- day06/src/bin/a.rs
+++ day06/src/bin/a.rs
@@ -1,18 +1,7 @@
-use std::collections::HashSet;
-
-use ascii::AsciiChar;
-use day06::parse_input;
+use day06::{parse_input, find_marker};
 
 fn main(){
     
     let input = parse_input();
-    for offset in 0..input.len(){
-        let marker_index = offset + 4;
-        let patch = &input[offset..marker_index];
-        let set = patch.chars().collect::<HashSet::<AsciiChar>>();
-        if set.len() == 4{
-            println!("{}", marker_index);
-            break;
-        }
-    }
+    println!("{}", find_marker(&input, 4));
 }
--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,4 +1,18 @@
-use ascii::{AsAsciiStr, AsciiStr, AsciiString};
+use std::collections::HashSet;
+
+use ascii::{AsAsciiStr, AsciiStr, AsciiString, AsciiChar};
+
+pub fn find_marker(input: &AsciiStr, marker_length: usize) -> usize{
+    for offset in 0..input.len(){
+        let marker_index = offset + marker_length;
+        let patch = &input[offset..marker_index];
+        let set = patch.chars().collect::<HashSet::<AsciiChar>>();
+        if set.len() == marker_length{
+            return marker_index;
+        }
+    }
+    panic!("No marker found");
+}
 
 pub fn parse_input() -> AsciiString{
     AsciiString::from(include_str!("../input.txt").as_ascii_str().unwrap())
